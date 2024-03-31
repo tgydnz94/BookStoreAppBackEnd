d8e6617910a64a020885bef03bef5927739f4e7f
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-03-31 19:06:24 +0300

    config

---
+++ Repositories/EfCore/Config/BookCOnfig.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EfCore.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData
+                (
+                    new Book { Id=1,Title="Karagöz ve Hacivat", Price=75}
+                );
+        }
+    }
+}
