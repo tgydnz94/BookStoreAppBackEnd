2ca94f91a3d095fcf515b4a993b4048c37ee19bf
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-04-07 18:27:44 +0300

    serviceIOC

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
 
 var app = builder.Build();
 
