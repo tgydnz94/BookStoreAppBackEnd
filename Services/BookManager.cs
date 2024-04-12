a1262679cf7ddb56c34a4cf2919bf29d02cc467e
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-04-12 05:12:13 +0300

    updates

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -12,15 +12,16 @@ namespace Services
     public class BookManager : IBookService
     {
         private readonly IRepositoryManager _manager;
+        private readonly ILoggerService _logger;
 
-        public BookManager(IRepositoryManager manager)
+        public BookManager(IRepositoryManager manager, ILoggerService logger)
         {
             _manager = manager;
+            _logger = logger;
         }
 
         public Book CreateOneBook(Book book)
         {
-            if (book is null) throw new ArgumentNullException(nameof (book));
 
             _manager.Book.CreateOneBook(book);
             _manager.Save();
@@ -31,7 +32,13 @@ namespace Services
         {
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
-            if (entity is null) throw new Exception($"Book with id: {id} could not found.");
+            if (entity is null)
+            {
+                string message = $"Book with id: {id} could not found.";
+
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
 
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
