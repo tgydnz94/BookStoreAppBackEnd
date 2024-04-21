88117aca5f7589ba84bedf0aa8dfa9c0f65b0de9
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-04-21 19:21:34 +0300

    filteringUpdate

--- Repositories/EfCore/BookRepository.cs
+++ Repositories/EfCore/BookRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Repositories.EfCore
 {
-    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    public sealed class BookRepository : RepositoryBase<Book>, IBookRepository
     {
         public BookRepository(RepositoryContext context) : base(context)
         {
@@ -23,10 +23,10 @@ namespace Repositories.EfCore
 
         public async Task<PagedList<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
-            var books = await FindByCondition(b=> 
-            ((b.Price >= bookParameters.MinPrice) && (b.Price <= bookParameters.MaxPrice)),trackChanges)
-           .OrderBy(b => b.Id)
-           .ToListAsync();
+            var books = await FindAll(trackChanges)
+                .FilterBooks(bookParameters.MinPrice, bookParameters.MaxPrice)
+                .OrderBy(b => b.Id)
+                .ToListAsync();
 
             return PagedList<Book>
                 .ToPagedList(books,
