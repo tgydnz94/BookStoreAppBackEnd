b2e8b7b8e52995f2cf69aa51f72a3c30d17005f4
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-04-25 23:39:31 +0300

    dataShaping

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -22,5 +22,6 @@ namespace Entities.RequestFeatures
             set { _pageSize = value > maxPageSize ? maxPageSize : value; }
         }
         public String? OrderBy { get; set; }
+        public String? Fields { get; set; }
     }
 }
